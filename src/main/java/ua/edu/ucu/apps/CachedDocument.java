2e4509a562e1af0e2be4651bac1c97a21b7c6731
ayrinnnx <kokhan.pn@ucu.edu.ua>
2023-12-23 18:41:01 +0200

    trying 5.0
    Signed-off-by: ayrinnnx <kokhan.pn@ucu.edu.ua>

--- src/main/java/ua/edu/ucu/apps/CachedDocument.java
+++ src/main/java/ua/edu/ucu/apps/CachedDocument.java
@@ -1,6 +1,11 @@
 package ua.edu.ucu.apps;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class CachedDocument implements Document {
     private Document document;
