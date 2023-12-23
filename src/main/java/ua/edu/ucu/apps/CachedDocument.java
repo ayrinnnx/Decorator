590818ca5a7c673be43f9a45a55b315d09c5971b
ayrinnnx <kokhan.pn@ucu.edu.ua>
2023-12-23 18:19:20 +0200

    trying
    Signed-off-by: ayrinnnx <kokhan.pn@ucu.edu.ua>

--- src/main/java/ua/edu/ucu/apps/CachedDocument.java
+++ src/main/java/ua/edu/ucu/apps/CachedDocument.java
@@ -1,2 +1,59 @@
-package ua.edu.ucu.apps;public class CachedDocument {
+package ua.edu.ucu.apps;
+
+import java.sql.*;
+
+public class CachedDocument implements Document {
+    private Document document;
+    private String identifier;
+    private String DB_URL = "jdbc:sqlite:/Users/my.bd";
+
+    public CachedDocument(String path, String documentIdentifier) {
+        this.document = new SmartDocument(path);
+        this.identifier = documentIdentifier;
+    }
+
+    @Override
+    public String parse() {
+        String tableRes = fromTable();
+
+        if (tableRes != null) {
+            return tableRes;
+        }
+
+        String result = document.parse();
+        toTable(result);
+        return result;
+    }
+
+    private void toTable(String result) {
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             Statement statement = connection.createStatement()) {
+            String query = "INSERT INTO table (identifier, result) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, identifier);
+            preparedStatement.setString(2, result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String fromTable() {
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             Statement statement = connection.createStatement()) {
+            String query = "SELECT result FROM table WHERE identifier = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, identifier);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getString("result");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
